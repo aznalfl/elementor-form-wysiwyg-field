52460b8f10d679652b021817778f98363316348f
Luke Lanza MBP <lukeflanza@gmail.com>
2025-06-23 22:13:31 +1000

    updating the class to use the content_template() method

--- form-fields/wysiwyg.php
+++ form-fields/wysiwyg.php
@@ -3,16 +3,13 @@ namespace EFS;
 
 use ElementorPro\Modules\Forms\Fields\Field_Base;
 
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 class Wysiwyg_Field extends Field_Base {
 
     public function __construct() {
         parent::__construct();
-        // Hook into the editor preview so we actually see a placeholder
-        add_action( 'elementor/preview/init', [ $this, 'editor_preview_init' ] );
+        // No need for wp_footer hacks any more
     }
 
     public function get_type(): string {
@@ -23,13 +20,8 @@ class Wysiwyg_Field extends Field_Base {
         return __( 'WYSIWYG', 'efs-wysiwyg' );
     }
 
-    /**
-     * Enqueue TinyMCE only when the form appears.
-     */
     public function get_script_depends(): array {
-        // Load WP’s built-in editor assets (TinyMCE + glue) and jQuery
         wp_enqueue_editor();
-        // Register our init stub
         wp_register_script(
             'efs-wysiwyg',
             plugins_url( '../assets/wysiwyg-field.js', __FILE__ ),
@@ -40,9 +32,6 @@ class Wysiwyg_Field extends Field_Base {
         return [ 'efs-wysiwyg' ];
     }
 
-    /**
-     * Render the textarea that TinyMCE will upgrade.
-     */
     public function render( $item, $item_index, $form ): void {
         $field_id = 'form-field-' . $item_index;
         $form->add_render_attribute( $field_id, [
@@ -55,36 +44,20 @@ class Wysiwyg_Field extends Field_Base {
         echo '<textarea ' . $form->get_render_attribute_string( $field_id ) . '></textarea>';
     }
 
-    /**
-     * Sanitise submitted HTML.
-     */
     public function validation( $field, $record, $ajax_handler ): void {
         $field['value'] = wp_kses_post( $field['value'] );
     }
 
     /**
-     * Set up an editor‐preview template so the WYSIWYG field
-     * shows in the Elementor canvas.
+     * Provide a live preview template in the Elementor editor.
      */
-    public function editor_preview_init() {
-        add_action( 'wp_footer', [ $this, 'content_template_script' ] );
-    }
-
-    public function content_template_script() {
+    public function content_template() {
         ?>
-        <script>
-        jQuery( document ).ready( () => {
-            elementor.hooks.addFilter(
-                'elementor_pro/forms/content_template/field/wysiwyg',
-                ( inputField, item, i ) => {
-                    const fieldId  = `form-field-${i}`;
-                    const classes  = `elementor-field elementor-wysiwyg-field ${ item.css_classes }`;
-                    return `<textarea id="${fieldId}" class="${classes}" rows="8" placeholder="WYSIWYG content…"></textarea>`;
-                },
-                10, 3
-            );
-        });
-        </script>
+        <textarea id="form-field-{{ data._sub_fields_index }}" 
+                  class="elementor-field elementor-wysiwyg-field {{ data.css_classes }}" 
+                  rows="8" 
+                  placeholder="<?php esc_attr_e( 'WYSIWYG content…', 'efs-wysiwyg' ); ?>">
+        </textarea>
         <?php
     }
 }
